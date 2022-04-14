c5a671c95659cdaef3eb666da3e08fafe0f31a88
with-developer <miki3079@naver.com>
2022-04-14 14:48:12 +0900

    form config

--- routes/form.js
+++ routes/form.js
@@ -1,21 +1,45 @@
 const express = require("express"); // require로 express 사용하겠음
 const router = express.Router(); // express 프레임워크 라우터를 사용하기위해 변수선언
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+	host: "127.0.0.1",
+	user: "root",
+	password: "sos7136@",
+	database: "vulnnode",
+});
 
 router.get("/", function (req, res, next) {
-	// 라우터는 URI 요청에 응답하는 방식을 말함
-	res.render("form", {
-		name: "보더코딩", // ejs 파일 의 <%%> 태그안에 담길 변수들
-		blog: "보더코딩의 블로그",
-		homepage: "보더코딩의 홈페이지",
-	}); // res.render 는 해당 'view' 파일을 지정할수잇음
+	res.render("form", { user: req.session.name });
 });
 
 router.post("/", function (req, res, next) {
-	// post 요청에 응당하는 router
-	res.json(req.body); // 요청받은데이터를 json 함수로 response 하겟음
+	var title = req.body.title;
+	var content = req.body.content;
+	var id = req.session.name;
+
+	console.log(title)
+	console.log(content)
+	console.log(id)
+
+	if (id === undefined) {
+		res.send(
+			"<script>alert('로그인 후 게시글을 작성하세요.');history.back();</script>"
+		);
+	}
+	else {
+		connection.query("insert into board (title, content, author) VALUES ?",
+			[[[title, content, id]]],
+			function (err, results, fields) {
+				if (err) console.log(err);
+				res.send(
+					"<script>alert('글 작성이 완료되었습니다.');history.back();</script>"
+				);
+			});
+	}
+
+
 });
 
-module.exports = router; // 전역으로 해당 라우터를 사용하게해줌
 
-
-
+module.exports = router;
