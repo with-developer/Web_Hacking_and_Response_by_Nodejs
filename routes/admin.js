240e26972cec457a6327fcb858760762ad0d8b30
kimg00n <kevin1226716@gmail.com>
2022-04-29 04:36:19 +0900

    어드민 페이지 구현완료

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const moment = require("moment");
 const mysql = require("mysql2");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
 const fs = require("fs");
 
@@ -114,14 +115,142 @@ router.get("/board/delete/:idx", function (req, res) {
 	);
 });
 
-router.get("/service", function (req, res, next) {
-	id = req.session.name;
+router.get("/service/:page", function (req, res, next) {
+	var id = req.session.name;
+	var page = req.params.page; // 현재 페이지는 params 을 req 요청받아옴
+	var sql = "select * from service order by _id desc"; // select 구절 그대로
 	if (id !== "admin") {
 		res.send(
 			"<script>alert('관리자페이지는 관리자만 접근가능합니다.');history.back();</script>"
 		);
 	} else {
-		res.render("admin_service");
+		connection.query(sql, function (err, rows) {
+			if (err) console.log("err : " + err);
+			res.render("admin_service", {
+				rows: rows,
+				page: page,
+				length: rows.length - 1,
+				page_num: 10,
+				pass: true,
+			});
+			// length 데이터 전체넘버 랜더링,-1을 한이유는 db에서는1부터지만 for문에서는 0부터 시작 ,page_num: 한페이지에 보여줄 갯수
+		});
 	}
+});
+
+router.get("/service/recognize/:idx", function (req, res) {
+	var id = req.session.name;
+	var idx = req.params.idx;
+	let username;
+	let email;
+
+	connection.query(
+		"SELECT * from service where _id=?",
+		[idx],
+		function (err, results) {
+			username = results[0].id;
+			connection.query(
+				"SELECT * FROM users WHERE id = ?",
+				[username],
+				function (err, results) {
+					if (err) console.error("err : " + err);
+					console.log(results[0]);
+					email = results[0].email;
+					console.log(email);
+					if (id !== "admin") {
+						res.send(
+							"<script>alert('권한이 없습니다.');history.back();</script>"
+						);
+					} else {
+						var sql = "DELETE FROM service WHERE _id = ?";
+						connection.query(sql, [idx], function (err, results) {
+							if (err) console.error("err : " + err);
+						});
+						res.send(
+							"<script>alert('메일이 전송되었습니다.');document.location.href='/admin/service/1';</script>"
+						);
+						const transporter = nodemailer.createTransport({
+							service: "gmail",
+							port: 465,
+							secure: true,
+							auth: {
+								user: "nodeapp000@gmail.com",
+								pass: process.env.GMAIL_PW,
+							},
+						});
+						const emailOptions = {
+							from: "nodeapp000@gmail.com",
+							to: email,
+							subject:
+								"닦다에서 서비스 예약 결과를 알려드립니다.",
+							html:
+								"<h1 >닦다에서 " +
+								username +
+								"님의 예약결과를 알려드립니다.</h1> <h2> 서비스 예약 승인되었습니다.</h2>",
+						};
+						transporter.sendMail(emailOptions, res);
+					}
+				}
+			);
+		}
+	);
+});
+
+router.get("/service/reject/:idx", function (req, res) {
+	var id = req.session.name;
+	var idx = req.params.idx;
+	let username;
+	let email;
+
+	connection.query(
+		"SELECT * from service where _id=?",
+		[idx],
+		function (err, results) {
+			username = results[0].id;
+			connection.query(
+				"SELECT * FROM users WHERE id = ?",
+				[username],
+				function (err, results) {
+					if (err) console.error("err : " + err);
+					console.log(results[0]);
+					email = results[0].email;
+					console.log(email);
+					if (id !== "admin") {
+						res.send(
+							"<script>alert('권한이 없습니다.');history.back();</script>"
+						);
+					} else {
+						var sql = "DELETE FROM service WHERE _id = ?";
+						connection.query(sql, [idx], function (err, results) {
+							if (err) console.error("err : " + err);
+						});
+						res.send(
+							"<script>alert('메일이 전송되었습니다.');document.location.href='/admin/service/1';</script>"
+						);
+						const transporter = nodemailer.createTransport({
+							service: "gmail",
+							port: 465,
+							secure: true,
+							auth: {
+								user: "nodeapp000@gmail.com",
+								pass: process.env.GMAIL_PW,
+							},
+						});
+						const emailOptions = {
+							from: "nodeapp000@gmail.com",
+							to: email,
+							subject:
+								"닦다에서 서비스 예약 결과를 알려드립니다.",
+							html:
+								"<h1 >닦다에서 " +
+								username +
+								"님의 예약결과를 알려드립니다.</h1> <h2> 서비스 예약 거절되었습니다.</h2>",
+						};
+						transporter.sendMail(emailOptions, res);
+					}
+				}
+			);
+		}
+	);
 });
 module.exports = router;
