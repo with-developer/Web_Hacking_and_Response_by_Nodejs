c4825a2018da50d60e88ee5d325f1415ce178f6d
kimg00n <kevin1226716@gmail.com>
2022-04-29 02:03:16 +0900

    어드민 페이지 회원관리 구현

--- routes/admin.js
+++ routes/admin.js
@@ -15,7 +15,7 @@ const connection = mysql.createConnection({
 router.get("/user/:page", function (req, res, next) {
 	var id = req.session.name;
 	var page = req.params.page; // 현재 페이지는 params 을 req 요청받아옴
-	var sql = "select * from board order by _id desc"; // select 구절 그대로
+	var sql = "select * from users order by _id desc"; // select 구절 그대로
 	if (id !== "admin") {
 		res.send(
 			"<script>alert('관리자페이지는 관리자만 접근가능합니다.');history.back();</script>"
@@ -29,12 +29,34 @@ router.get("/user/:page", function (req, res, next) {
 				length: rows.length - 1,
 				page_num: 10,
 				pass: true,
-				moment,
-				user: req.session.name,
 			});
 			// length 데이터 전체넘버 랜더링,-1을 한이유는 db에서는1부터지만 for문에서는 0부터 시작 ,page_num: 한페이지에 보여줄 갯수
 		});
 	}
+});
+
+router.get("/user/delete/:idx", function (req, res) {
+	var id = req.session.name;
+	var idx = req.params.idx;
+	connection.query(
+		"SELECT * from users where _id=?",
+		[idx],
+		function (err, results) {
+			if (id !== "admin") {
+				res.send(
+					"<script>alert('권한이 없습니다.');history.back();</script>"
+				);
+			} else {
+				var sql = "DELETE FROM users WHERE _id = ?";
+				connection.query(sql, [idx], function (err, results) {
+					if (err) console.error("err : " + err);
+					res.send(
+						"<script>alert('회원 삭제가 완료되었습니다.');document.location.href='/admin/user/1';</script>"
+					);
+				});
+			}
+		}
+	);
 });
 
 router.get("/board/:page", function (req, res, next) {
